62fb6f45d834af266a84cf286751a8daf7a1c220
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-06-03 05:44:02 +0900

    플레이어 화면에 띄우기 및 애니메이션 적용
    IDLE 애니메이션 적용

--- Twin_Shot_Reborn/Twin_Shot_Reborn/PlayerObject.cpp
+++ Twin_Shot_Reborn/Twin_Shot_Reborn/PlayerObject.cpp
@@ -1,7 +1,19 @@
 #include "PlayerObject.h"
 
+#include <cmath>
+
 PlayerObject::PlayerObject()
 {
+	pos = { 600, 450 };
+	size = { 100, 100 };
+	// 이미지 로드
+	images["Idle"].resize(4);
+	for (int i = 0; i < 4; ++i) {
+		images["Idle"][i].Load(L"./Resources/Images/Character/Idle/" + std::to_wstring(i + 1) + L".png");
+	}
+
+	framePerSecond = 8.5f;
+	nowFrameIdxF = 0.f;
 }
 
 PlayerObject::~PlayerObject()
@@ -10,8 +22,11 @@ PlayerObject::~PlayerObject()
 
 void PlayerObject::update(float elapsedTime)
 {
+	nowFrameIdxF += framePerSecond * elapsedTime;
+	nowFrameIdxF = fmod(nowFrameIdxF, float(images["Idle"].size()));
 }
 
 void PlayerObject::draw(HDC hdc)
 {
+	images["Idle"][int(nowFrameIdxF)].MyDraw(hdc, getObjectRect());
 }
