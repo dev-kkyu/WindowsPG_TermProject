4dc08d793c6214e2ba8343ef74f457c14b852274
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-06-10 03:48:12 +0900

    MyImage 클래스 함수 추가
    srcRect 받을 수 있도록 수정 및 Alpha값 받도록 수정 -> alpha 이미지가 아닌경우에 대하여 고민 필요

--- Twin_Shot_Reborn/Twin_Shot_Reborn/MyImage.cpp
+++ Twin_Shot_Reborn/Twin_Shot_Reborn/MyImage.cpp
@@ -27,7 +27,7 @@ HRESULT MyImage::Load(std::wstring filename)
 	return image->Load(filename.c_str());
 }
 
-void MyImage::MyDraw(HDC hdc, const RECT& dstRect, bool reverseX, bool reverseY) const
+void MyImage::MyDraw(HDC hdc, const RECT& dstRect, bool reverseX, bool reverseY, BYTE bSrcAlpha) const
 {
 	const int imgWidth = image->GetWidth();
 	const int imgHeight = image->GetHeight();
@@ -55,10 +55,10 @@ void MyImage::MyDraw(HDC hdc, const RECT& dstRect, bool reverseX, bool reverseY)
 
 		// 복사한 이미지를 화면에 그린다
 		BLENDFUNCTION blend;
-		blend.BlendOp = AC_SRC_OVER;		// 유일
-		blend.BlendFlags = 0;				// 유일
-		blend.SourceConstantAlpha = 0xff;	// 0(투명) ~ 255(불투명)	// 기본적으로 투명 채널 존재시 투명도 반영된다.
-		blend.AlphaFormat = AC_SRC_ALPHA;	// AC_SRC_ALPHA -> 32비트 비트맵일 때
+		blend.BlendOp = AC_SRC_OVER;			// 유일
+		blend.BlendFlags = 0;					// 유일
+		blend.SourceConstantAlpha = bSrcAlpha;	// 0(투명) ~ 255(불투명)	// 기본적으로 투명 채널 존재시 투명도 반영된다.
+		blend.AlphaFormat = AC_SRC_ALPHA;		// AC_SRC_ALPHA -> 32비트 비트맵일 때
 		::AlphaBlend(hdc, dstRect.left, dstRect.top, dstRect.right - dstRect.left, dstRect.bottom - dstRect.top, mDC, 0, 0, imgWidth, imgHeight, blend);
 
 		// 만든 오브젝트 삭제
@@ -82,7 +82,67 @@ void MyImage::MyDraw(HDC hdc, const RECT& dstRect, bool reverseX, bool reverseY)
 	}
 }
 
-void MyImage::MyDraw(HDC hdc, const RECT& dstRect, int windowLeft, bool reverseX, bool reverseY) const
+void MyImage::MyDraw(HDC hdc, const RECT& dstRect, int windowLeft, bool reverseX, bool reverseY, BYTE bSrcAlpha) const
 {
 	MyDraw(hdc, RECT{ dstRect.left - windowLeft , dstRect.top, dstRect.right - windowLeft, dstRect.bottom }, reverseX, reverseY);
+}
+
+void MyImage::MyDraw(HDC hdc, const RECT& dstRect, const RECT& srcRect, bool reverseX, bool reverseY, BYTE bSrcAlpha) const
+{
+	const int imgWidth = srcRect.right - srcRect.left;
+	const int imgHeight = srcRect.bottom - srcRect.top;
+
+	// Alpha 채널을 가진 경우
+	if (32 == image->GetBPP()) {
+		// 좌우/상하 반전 효과를 위한 중간 메모리 DC 생성
+		HDC mDC = ::CreateCompatibleDC(hdc);
+		HBITMAP hBitmap = ::CreateCompatibleBitmap(hdc, imgWidth, imgHeight);
+		::SelectObject(mDC, hBitmap);
+
+		// 좌우, 상하 반전을 위해 메모리 DC에 복사한다.
+		if (reverseX and reverseY) {
+			image->StretchBlt(mDC, RECT{ 0, 0, imgWidth, imgHeight }, RECT{ srcRect.right,srcRect.bottom, srcRect.left, srcRect.top }, SRCCOPY);
+		}
+		else if (reverseX) {
+			image->StretchBlt(mDC, RECT{ 0, 0, imgWidth, imgHeight }, RECT{ srcRect.right, srcRect.top, srcRect.left, srcRect.bottom }, SRCCOPY);
+		}
+		else if (reverseY) {
+			image->StretchBlt(mDC, RECT{ 0, 0, imgWidth, imgHeight }, RECT{ srcRect.left, srcRect.bottom, srcRect.right, srcRect.top }, SRCCOPY);
+		}
+		else {
+			image->BitBlt(mDC, srcRect.left, srcRect.top, SRCCOPY);
+		}
+
+		// 복사한 이미지를 화면에 그린다
+		BLENDFUNCTION blend;
+		blend.BlendOp = AC_SRC_OVER;			// 유일
+		blend.BlendFlags = 0;					// 유일
+		blend.SourceConstantAlpha = bSrcAlpha;	// 0(투명) ~ 255(불투명)	// 기본적으로 투명 채널 존재시 투명도 반영된다.
+		blend.AlphaFormat = AC_SRC_ALPHA;		// AC_SRC_ALPHA -> 32비트 비트맵일 때
+		::AlphaBlend(hdc, dstRect.left, dstRect.top, dstRect.right - dstRect.left, dstRect.bottom - dstRect.top, mDC, 0, 0, imgWidth, imgHeight, blend);
+
+		// 만든 오브젝트 삭제
+		::DeleteObject(hBitmap);
+		::DeleteDC(mDC);
+	}
+	else {
+		// 알파 채널이 없는 경우 바로 그린다
+		if (reverseX and reverseY) {
+			image->StretchBlt(hdc, dstRect, RECT{ srcRect.right,srcRect.bottom, srcRect.left, srcRect.top }, SRCCOPY);
+		}
+		else if (reverseX) {
+			image->StretchBlt(hdc, dstRect, RECT{ srcRect.right, srcRect.top, srcRect.left, srcRect.bottom }, SRCCOPY);
+		}
+		else if (reverseY) {
+			image->StretchBlt(hdc, dstRect, RECT{ srcRect.left, srcRect.bottom, srcRect.right, srcRect.top }, SRCCOPY);
+		}
+		else {
+			image->StretchBlt(hdc, dstRect, srcRect, SRCCOPY);
+		}
+	}
+}
+
+void MyImage::MyDraw(HDC hdc, const RECT& dstRect, const RECT& srcRect, int windowLeft, bool reverseX, bool reverseY, BYTE bSrcAlpha) const
+{
+	MyDraw(hdc, RECT{ dstRect.left - windowLeft , dstRect.top, dstRect.right - windowLeft, dstRect.bottom }, srcRect, reverseX, reverseY);
 }
