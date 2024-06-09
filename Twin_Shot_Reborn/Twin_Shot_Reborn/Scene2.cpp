218939438b4757b4ec703ee132c13e673991b08b
HyunjungWoo <112833211+HyunjungWoo@users.noreply.github.com>
2024-06-09 22:18:20 +0900

    Scene2 - nonBlock 생성 및 위치조정

--- Twin_Shot_Reborn/Twin_Shot_Reborn/Scene2.cpp
+++ Twin_Shot_Reborn/Twin_Shot_Reborn/Scene2.cpp
@@ -6,7 +6,39 @@ Scene2::Scene2()
 
 
 	// nonBlockTiles 배치
-	nonBlockTiles.emplace_back(POINT{ 75, 151 }, 25); // 꽃
+	for (int i = 0; i < 2; i++) {
+		// 핑크 꽃 - 왼쪽
+		nonBlockTiles.emplace_back(POINT{ 75,  151 + i * 400 }, 25);
+		nonBlockTiles.emplace_back(POINT{ 125, 151 + i * 400 }, 24);
+		nonBlockTiles.emplace_back(POINT{ 175, 151 + i * 400 }, 26);
+		nonBlockTiles.emplace_back(POINT{ 225, 151 + i * 400 }, 24);
+		nonBlockTiles.emplace_back(POINT{ 275, 151 + i * 400 }, 25);
+
+		nonBlockTiles.emplace_back(POINT{ 1125, 351 + i * 400 }, 25);
+		nonBlockTiles.emplace_back(POINT{ 1175, 351 + i * 400 }, 24);
+		nonBlockTiles.emplace_back(POINT{ 1225, 351 + i * 400 }, 26);
+		nonBlockTiles.emplace_back(POINT{ 1275, 351 + i * 400 }, 24);
+		nonBlockTiles.emplace_back(POINT{ 1325, 351 + i * 400 }, 25);
+
+		// 파란 꽃 - 왼쪽
+		nonBlockTiles.emplace_back(POINT{ 75,  351 + i * 400 }, 29);
+		nonBlockTiles.emplace_back(POINT{ 125, 351 + i * 400 }, 30);
+		nonBlockTiles.emplace_back(POINT{ 175, 351 + i * 400 }, 31);
+		nonBlockTiles.emplace_back(POINT{ 225, 351 + i * 400 }, 30);
+		nonBlockTiles.emplace_back(POINT{ 275, 351 + i * 400 }, 29);
+
+		nonBlockTiles.emplace_back(POINT{ 1125, 151 + i * 400 }, 29);
+		nonBlockTiles.emplace_back(POINT{ 1175, 151 + i * 400 }, 30);
+		nonBlockTiles.emplace_back(POINT{ 1225, 151 + i * 400 }, 31);
+		nonBlockTiles.emplace_back(POINT{ 1275, 151 + i * 400 }, 30);
+		nonBlockTiles.emplace_back(POINT{ 1325, 151 + i * 400 }, 29);
+	}
+
+	// nonBlock 잡초 
+	nonBlockTiles.emplace_back(POINT{ 525,  151 }, 27);
+	nonBlockTiles.emplace_back(POINT{ 925,  401 }, 27);
+	nonBlockTiles.emplace_back(POINT{ 725,  651 }, 27);
+
 
 	// 타일 배치 (가로 28, 세로 22)
 	for (int i = 0; i < 28; ++i) { // 가로 
@@ -36,7 +68,7 @@ Scene2::Scene2()
 
 	// 세로 
 	for (int i = 0; i < 22; ++i) {
-		tiles.emplace_back(POINT{ 25, 50 + i * 50 }, rand()%3 + 20);
+		tiles.emplace_back(POINT{ 25, 50 + i * 50 }, rand() % 3 + 20);
 		tiles.emplace_back(POINT{ 1375, 50 + i * 50 }, rand() % 3 + 20);
 	}
 
